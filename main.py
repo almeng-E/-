ee70a947b4915ad9302a34d6755e33584eaa5b23
Wonmin Choi <min5024@naver.com>
2024-06-08 01:18:30 +0900

    검색기록 기능 추가

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def show_reason(food_name):
     else:
         reasons = haram_reasons
     print(f'{food_name.upper()} contains the following haram ingredients :')
-    for i, idx in enumerate(reasons[food_name]):
-        print(f'{i+1}.  {ingredients[idx]}')
-    print('--------------------------------------------------------------------------')
+    for i, v in enumerate(reasons[food_name]):
+        print(f'{i+1}.  {ingredients[v]}')
+    # print('--------------------------------------------------------------------------')
 
 
 
 
 if __name__ == "__main__":
+    # 검색기록 빈 리스트 생성
+    searched_history = []
+    # 프로그램 시작
     print("==========================================================================")
     print("---------------------------K-Food Halal Checker---------------------------")
     print()
@@ -26,12 +29,15 @@ if __name__ == "__main__":
     print("!! This program will help you to check whether the food is halal or not")
     
     while True:
+        print('--------------------------------------------------------------------------')
         print()
         print("┌──────────────────────────────────────────────────────────────────────┐")
         print("│ Please choose an option:                                             │")
         print("│                                                                      │")
         print("│  [1]  Check Food                                                     │")
         print("│  [2]  Check Brand                                                    │")
+        print("│  [3]  View Search History                                            │")
+        print("│                                                                      │")
         print("│  [0]  Exit                                                           │")
         print("└──────────────────────────────────────────────────────────────────────┘")
         btn = input('- Enter your choice: ')
@@ -43,11 +49,15 @@ if __name__ == "__main__":
 
         elif btn == '1':
             food_name = input('Please enter the food name : ')
-            food_name = food_name.replace(" ","").lower()   # 입력한 음식이 띄어쓰기와 대소문자 구분 없이 검색되도록 소문자로 변환
+            # 입력한 음식이 띄어쓰기와 대소문자 구분 없이 검색되도록 소문자로 변환
+            food_name = food_name.replace(" ","").lower()   
             if food_name not in foods_dict:
                 print('This food is not in the list')
                 print('BACK TO MAIN MENU')
                 continue
+            # 검색 기록에 추가
+            searched_history.append(("Food", food_name))
+            # 할랄 여부 출력
             if foods_dict[food_name] == 'a':
                 print('This food is halal')
                 print('Enjoy your meal! :)')
@@ -60,18 +70,36 @@ if __name__ == "__main__":
 
         elif btn == '2':
             brand_name = input('Please enter the brand name : ')
-            brand_name_changed = brand_name.replace(" ","").lower()   # 입력한 브랜드가 띄어쓰기와 대소문자 구분 없이 검색되도록 소문자로 변환
+            # 입력한 브랜드가 띄어쓰기와 대소문자 구분 없이 검색되도록 소문자로 변환
+            brand_name_changed = brand_name.replace(" ","").lower()   
             if brand_name_changed not in brands_dict:
                 print('This brand is not in the list')
                 print('BACK TO MAIN MENU')
                 continue
+            # 검색 기록에 추가
+            searched_history.append(("Brand", brand_name))
+            # 브랜드의 대표 메뉴 및 할랄 여부 출력
             print(f'Here are two best selling menus from {brand_name}.')
             # print(brands_dict[brand_name_changed])
             for item, status in brands_dict[brand_name_changed].items():
                 status_str = 'Halal' if status == 1 else 'Haram'
                 print(f"+ {item} : {status_str}")
             print('For more information on other menus, try our food name checking service.')
-            print('--------------------------------------------------------------------------')
+            # print('--------------------------------------------------------------------------')
+
+        elif btn == '3':   
+            print('Search History')
+            if searched_history == []:
+                print('No search history')
+                print('BACK TO MAIN MENU')
+
+            else:
+                print('Here is your search history')
+                for i in range(len(searched_history)):
+                    # print(f'[{i+1}] {searched_history[i][0]} : {searched_history[i][1]}')
+                    print(f'[{i+1:<2}] {searched_history[i][0]:<6} : {searched_history[i][1]:<20}')
+
+        # TODO : 검색기록 삭제 기능 추가 , 검색기록에서 바로 검색 기능 추가 
 
         else:
             print('Please enter [0], [1], or [2] only.')
